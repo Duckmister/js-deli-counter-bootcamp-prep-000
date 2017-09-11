5aef8e46381b4e3474513e44846018a7728c0038
T <theresa.hindersinn@gmail.com>
2017-09-11 22:15:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ function currentLine(line){
   if (line.length === 0){
     return "The line is currently empty.";
   }else{
+    var array = [];
     for(var i= 1; line.length > 0; i++){
-      console.log('The line is currently: ' + i + line;)
+      array.push(i + line[i]);
     }
+    return 'The line is currently: ' + array;
   }
 }
